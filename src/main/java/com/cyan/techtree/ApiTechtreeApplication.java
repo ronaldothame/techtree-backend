7a746440ff3ece5941b607062ecf1e45de5fd3c8
rthame <ronaldothame1@gmail.com>
2024-03-19 11:06:51 -0300

    first commit

---
+++ src/main/java/com/cyan/techtree/ApiTechtreeApplication.java
@@ -0,0 +1,13 @@
+package com.cyan.techtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTechtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTechtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyan/techtree/ApiTechtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyan.techtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTechtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
